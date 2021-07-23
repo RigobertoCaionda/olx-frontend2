912c3f6a5d9bfe380d7127a97a15eb83974b2068
Rigoberto Caionda <rigobertocaionda98@gmail.com>
2021-07-23 09:18:38 +0100

    Meu primeiro teste

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [nome, setNome] = useState('Fulano');
+  const handleButton = ()=>{
+  setNome('Rigoberto');
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>Oi, tudo bem {nome}? </div>
+      <button onClick={handleButton}>Mudar o nome</button>
+      </>
   );
 }
 
